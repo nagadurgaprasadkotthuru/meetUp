06133d0ce1312e710ff6253271e433469880e555
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-06-15 21:21:28 +0530

    second_commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
+import {Component} from 'react'
+
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import MeetupContext from './context/MeetupContext'
+
+import Home from './components/Home'
+import Register from './components/Register'
+import NotFound from './components/NotFound'
+
 import './App.css'
 
 // These are the lists used in the application. You can move them to any component needed.
-const topicsList = [
-  {
-    id: 'ARTS_AND_CULTURE',
-    displayText: 'Arts and Culture',
-  },
-  {
-    id: 'CAREER_AND_BUSINESS',
-    displayText: 'Career and Business',
-  },
-  {
-    id: 'EDUCATION_AND_LEARNING',
-    displayText: 'Education and Learning',
-  },
-  {
-    id: 'FASHION_AND_BEAUTY',
-    displayText: 'Fashion and Learning',
-  },
-  {
-    id: 'GAMES',
-    displayText: 'Games',
-  },
-]
 
 // Replace your code here
-const App = () => <h1>Hello World</h1>
+class App extends Component {
+  state = {
+    name: '',
+    activeTopic: 'Arts and Culture',
+    isShowRegisteredView: false,
+    isShowErrorMsg: false,
+  }
+
+  onChangeInput = event => this.setState({name: event.target.value})
+
+  onChangeTopic = event => this.setState({activeTopic: event.target.value})
+
+  onChangeIsShowRegisteredView = () =>
+    this.setState({isShowRegisteredView: true})
+
+  onChangeIsShowErrorMsg = () => this.setState({isShowErrorMsg: true})
+
+  render() {
+    const {name, activeTopic, isShowRegisteredView, isShowErrorMsg} = this.state
+    return (
+      <MeetupContext.Provider
+        value={{
+          name,
+          activeTopic,
+          isShowRegisteredView,
+          isShowErrorMsg,
+          onChangeInput: this.onChangeInput,
+          onChangeIsShowRegisteredView: this.onChangeIsShowRegisteredView,
+          onChangeIsShowErrorMsg: this.onChangeIsShowErrorMsg,
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </MeetupContext.Provider>
+    )
+  }
+}
 
 export default App
