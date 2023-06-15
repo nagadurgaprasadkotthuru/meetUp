923973ae68dede5bec14d1a4211e2dd1acd4b5fe
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-06-15 21:33:35 +0530

    second_commit

--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -3,3 +3,32 @@ import styled from 'styled-components'
 export const BgContainer = styled.div`
   min-height: 100vh;
 `
+
+export const ContentContainer = styled.div`
+  min-height: 90vh;
+`
+
+export const Heading = styled.h1`
+  color: ${props => (props.active ? '#334155' : '#2563eb')};
+  font-family: 'Roboto';
+  font-size: 46px;
+`
+
+export const Description = styled.p`
+  color: ${props => (props.active ? '#475569' : '#334155')};
+  font-family: 'Roboto';
+  font-size: 20px;
+`
+
+export const Image = styled.image`
+  width: 20%;
+`
+
+export const Button = styled.button`
+  font-family: 'Roboto';
+  font-size: 14px;
+  background-color: #3b82f6;
+  border: 0px;
+  border-radius: 6px;
+  padding: 10px 25px 10px 25px;
+`
