d70d960ffc7379c0e58219d5ce9ccf55c10970cc
nagadurgaprasadkotthuru <125784996+nagadurgaprasadkotthuru@users.noreply.github.com>
2023-06-15 21:32:24 +0530

    second_commit

---
+++ src/components/Register/styledComponents.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  min-height: 100vh;
+`
+
+export const ContentContainer = styled.div`
+  min-height: 90vh;
+`
+
+export const Image = styled.image`
+  width: 20%;
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  height: 360px;
+  width: 20%;
+`
+
+export const Heading = styled.h1`
+  color: #334155;
+  font-family: 'Roboto';
+  font-size: 28px;
+`
+
+export const Label = styled.label`
+  color: #64748b;
+  font-family: 'Roboto';
+  font-size: 16px;
+`
+
+export const Input = styled.input`
+  width: 100%;
+  height: 38px;
+  border: 1px solid #7b8794;
+  border-radius: 6px;
+  font-family: 'Roboto';
+  color: #7b8794;
+  padding-left: 10px;
+`
+
+export const Select = styled.select`
+  width: 100%;
+  height: 38px;
+  border: 1px solid #7b8794;
+  border-radius: 6px;
+  font-family: 'Roboto';
+  color: #334155;
+  padding-left: 10px;
+`
+
+export const Option = styled.option`
+  font-family: 'Roboto';
+  font-size: 16px;
+  color: #334155;
+`
+
+export const Button = styled.button`
+  font-family: 'Roboto';
+  font-size: 14px;
+  background-color: #3b82f6;
+  border: 0px;
+  border-radius: 6px;
+  padding: 10px 25px 10px 25px;
+`
